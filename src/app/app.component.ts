532a7a2f02733105ddd0845cec641b83bfec42ae
RanjithKumar-RK7 <ranjith.kumar@bpmlinks.com>
2021-03-19 11:49:55 +0530

    Added Image Cropper to get co-ords from Image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { ImageCroppedEvent } from 'ngx-image-cropper';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Invoice-UI';
+  imageChangedEvent: any = '';
+  croppedImage: any = '';
+  positions: any = '';
+  initialPosition: any = { x1: 0, x2: 100, y1: 0, y2: 100 };
+
+  fileChangeEvent(event: any): void {
+    this.imageChangedEvent = event;
+  }
+
+  imageCropped(event: ImageCroppedEvent) {
+    this.croppedImage = event.base64;
+    this.positions = event.imagePosition;
+    console.log(
+      'This position needs to be stored in the database. Will be sent to backend.'
+    );
+    console.log(event.imagePosition);
+  }
+
+  imageLoaded() {
+    console.log('Image Loaded Sucessfully');
+  }
+
+  cropperReady() {
+    console.log('Cropper Ready');
+  }
+
+  loadImageFailed() {
+    console.log('Image Load Failed');
+    alert('Image failed to load. Please try again');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 // import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { ImageCropperModule } from 'ngx-image-cropper';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // PdfViewerModule
+    // PdfViewerModule,
+    ImageCropperModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
